bcc8ba9ca1c26ab735904696c30e233d4704c108
nvnamsss <nvnam.c@gmail.com>
2020-03-23 15:06:02 +0700

    completed hierachy base and logic

--- Intratech/Main.cs
+++ Intratech/Main.cs
@@ -19,31 +19,56 @@ namespace Assets.Intratech
             Debug.Log("Hi mom2");
             view.transform.SetParent(Parent.transform);
             view.name = root.Value;
-            view.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            RectTransform rect = (RectTransform)view.transform;
+            Vector2 size = rect.sizeDelta;
+            size.x = 128;
+            size.y = 32;
+            rect.sizeDelta = size;
+
+            //view.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
 
             HierachyData node1 = new HierachyData("node1");
             root.Add(node1);
             UIHierachy view1 = UIHierachy.Create(HierachyBase, node1);
-            view1.name = node1.Value;
-            view1.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            rect = (RectTransform)view1.transform;
+            size = rect.sizeDelta;
+            size.x = 128;
+            size.y = 32;
+            rect.sizeDelta = size;
+            //view1.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
 
             HierachyData node2 = new HierachyData("node2");
             root.Add(node2);
             UIHierachy view2 = UIHierachy.Create(HierachyBase, node2);
             view2.name = node2.Value;
-            view2.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            rect = (RectTransform)view2.transform;
+            size = rect.sizeDelta;
+            size.x = 128;
+            size.y = 32;
+            rect.sizeDelta = size;
+            //view2.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
 
             HierachyData node21 = new HierachyData("node21");
             node2.Add(node21);
             UIHierachy view21 = UIHierachy.Create(HierachyBase, node21);
             view21.name = node21.Value;
-            view21.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            rect = (RectTransform)view21.transform;
+            size = rect.sizeDelta;
+            size.x = 128;
+            size.y = 32;
+            rect.sizeDelta = size;
+            //view21.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
 
             HierachyData node3 = new HierachyData("node3");
             root.Add(node3);
             UIHierachy view3 = UIHierachy.Create(HierachyBase, node3);
             view3.name = node3.Value;
-            view3.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            rect = (RectTransform)view3.transform;
+            size = rect.sizeDelta;
+            size.x = 128;
+            size.y = 32;
+            rect.sizeDelta = size;
+            //view3.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
         }
     }
 }
